bc965339cc6371aca9a0c0868d4b3be5bc6cae9e
IDarkstarX <90350962+IDarkstarX@users.noreply.github.com>
2021-09-17 16:21:12 -0500

    intial

---
+++ Assets/scripts/mover.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour {
+
+    [SerializeField]
+    float moveSpeed = 1;
+    [SerializeField]
+    float jumpSpeed = 100;
+
+    Rigidbody rb;
+
+    bool canJump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+        Ray r = new Ray(transform.position, Vector3.down);
+
+        RaycastHit hit;
+        
+        Debug.DrawLine(r.origin, r.origin + Vector3.down * 3);
+
+        if(Physics.Raycast(r, out hit, 3))
+        {
+            if(hit.transform.tag == "walkable" && hit.transform != null)
+            {
+                canJump = true;
+                
+            } else
+            {
+                canJump = false;
+            }
+            Debug.Log(hit.transform.name);
+        }
+        
+        Debug.Log(canJump);
+
+        //mobility//
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(h * moveSpeed, rb.velocity.y, v * moveSpeed);
+
+        if(Input.GetButtonDown("Jump") && canJump) {
+
+            Jump();
+        }
+    }
+    void Jump() {
+
+        rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, rb.velocity.y);
+        canJump = false;
+    }
+}
